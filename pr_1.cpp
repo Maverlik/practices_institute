5fb434ee565930c1499b9b89b2ec1ec874ea20a5
Alexey <91792454+Maverlik@users.noreply.github.com>
2023-04-01 13:58:16 +0300

    Add files via upload

---
+++ pr_1.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+struct Record {
+    string name;
+    char type;
+    float price;
+    int amount;
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Record Table[10];
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Введите название товара>>";
+        cin >> Table[i].name;
+        cout << "Введите тип товара>>";
+        cin >> Table[i].type;
+        cout << "Введите цену товара>>";
+        cin >> Table[i].price;
+        cout << "Введите минимальное количество товара>>";
+        cin >> Table[i].amount;
+    }
+
+    cout << "|------------------------------------------------------------------------------------------|" << endl;
+    cout.width(60);
+    cout << "|Прайс-лист                                                                                |" << endl;
+    cout << "|------------------------------------------------------------------------------------------|" << endl;
+    cout << "|Наименование товара |  Тип товара  | Цена за 1 шт (грн) |  Минимальное количество в партии|" << endl;
+    cout << "|------------------------------------------------------------------------------------------|" << endl;
+
+    for (int i = 0; i < 3; i++) {
+        cout << "|";
+        cout.width(20);
+        cout << left << Table[i].name;
+        cout << "|";
+        cout.width(14);
+        cout << left << Table[i].type;
+        cout << "|";
+        cout.width(20);
+        cout << left << Table[i].price;
+        cout << "|";
+        cout.width(33);
+        cout << left << Table[i].amount;
+        cout << "|" << endl;
+        cout << "|------------------------------------------------------------------------------------------|" << endl;
+    }
+
+    cout << "|Примечание: К – канцтовары, О - оргтехника                                                |" << endl;
+    cout << "|------------------------------------------------------------------------------------------|" << endl;
+
+    return 0;
+}
---
+++ pr_2.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+struct Record {
+    string name;
+    char type;
+    float price;
+    int amount = 0;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Record records[10];
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Введите название товара: ";
+        cin >> records[i].name;
+        cout << "Введите тип товара (О/К): ";
+        cin >> records[i].type;
+        cout << "Введите цену товара: ";
+        cin >> records[i].price;
+        cout << "Введите минимальное количество товара: ";
+        cin >> records[i].amount;
+    }
+
+    Record* A = (Record*)malloc(3 * sizeof(Record));
+    if (A == NULL) {
+        cout << "Ошибка при выделении памяти" << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        *(A + i) = records[i];
+    }
+
+    Record* tmp = (Record*)realloc(A, 10 * sizeof(Record));
+    if (tmp == NULL) {
+        cout << "Ошибка при изменении размера памяти" << endl;
+        free(A);
+        return 1;
+    }
+    else {
+        A = tmp;
+    }
+
+    for (int i = 3; i < 10; i++) {
+        A[i].name = "Unknown";
+        A[i].type = '-';
+        A[i].price = 0.0f;
+        A[i].amount = 0;
+    }
+
+    Record* B = new Record[10];
+    for (int i = 0; i < 10; i++) {
+        *(B + i) = *(A + i);
+    }
+
+    cout << "Адрес первого элемента массива Table: " << &records[0] << endl;
+    cout << "Адрес первого элемента массива A: " << A << endl;
+    cout << "Адрес первого элемента массива B: " << B << endl;
+
+
+    cout << "|---------------|--------|--------------|-----------|" << endl;
+    cout << "|";
+    cout.width(20); cout << left << "Адрес";
+    cout.width(14); cout << left << "|Название";
+    cout.width(20); cout << left << "|Адрес";
+    cout.width(20); cout << left << "|Название";
+    cout.width(33); cout << left << "|" << endl;
+    cout << "|---------------|--------|--------------|-----------|" << endl;
+
+    for (int i = 0; i < 3; i++) {
+        cout << "|";
+        cout.width(15); cout << left << (A + i);
+        cout.width(9); cout << left << "|" + (A + i)->name;
+        cout << "|";
+        cout.width(14); cout << left << (B + i);
+        cout.width(12); cout << left << "|" + (B + i)->name;
+        cout << "|" << endl;
+        cout << "|---------------|--------|--------------|-----------|" << endl;
+    }
+
+    free(A);
+    delete[] B;
+
+    return 0;
+}
